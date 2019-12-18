97b4fb49f8e40ad637021308a3b86f429324aff1
kaustubhvkhairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2019-12-19 04:40:03 +0530

    Create Num.py

---
+++ Assignment4/Num.py
@@ -0,0 +1,10 @@
+def chkprime(no):
+    flag = 0;
+    for i in range(2, no ):
+        if (no % i == 0):
+            flag = 1;
+            break;
+    if (flag == 1):
+        return False;
+    else:
+        return True;
