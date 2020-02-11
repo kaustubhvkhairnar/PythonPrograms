acac5cc2eee9b69322b9bc410b7e4cb2fb903f3c
Kaustubh Khairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2020-02-11 21:32:17 +0530

    Update Assignment9_3.py

--- Assignment9/Assignment9_3.py
+++ Assignment9/Assignment9_3.py
@@ -4,8 +4,8 @@
 
 import os;
 from sys import *;
-
-
+from termcolor import colored, cprint 
+from colorama import Fore
 def read(file):
     exists = os.path.exists(file);
     if exists:
@@ -15,22 +15,51 @@ def read(file):
         nobj.close();
         fobj.close();
         print()
-        print("Your data is successfully copied from '" + file + "' to 'Demo.txt'")
+        print("Your data is successfully copied from '" + file + "' to 'Demo.txt'\n")
     else:
         print("No such File in directory");
         print()
-        print("------------------USAGE------------------")
-        print("python Application.py Filename.extension")
-        print("------------------------------------------")	
+        #displayhelp()
         
-    
-
+ '''       
+def displayhelp():
+	line = '-'*43
+	cprint("\n------------------",'yellow',end=' ')
+	print("HELP",end=' ')
+	cprint("------------------\n",'yellow')
+	print("This Script is used to copy the contents of one file into another file.\n")
+	cprint(line,'yellow')
+	cprint("\n-----------------",'yellow',end=' ')
+	print("USAGE",end=' ')
+	cprint("------------------\n",'yellow')
+	print("python ApplicationName.py Filename.extension\n")
+	cprint(line,'yellow')
+	cprint("\n----------------",'yellow',end=' ')
+	print("EXAMPLE",end=' ')
+	cprint("------------------\n",'yellow')
+	print("python CopyContents.py hello.py\n")
+	cprint(line,'yellow')	
+	print()
+'''	
 def main():
-    read(argv[1]);    
+	line = '-'*43
+	print()
+	cprint(line,'blue')
+	print(colored('Designed and Developed by Kaustubh Khairnar', 'yellow', 'on_blue')) 
+	cprint(line,'blue')
 	
-    print()
-    print("Thank you for using our script")
-    print()
-
+	try:
+		read(argv[1])
+		
+	except Exception as E:
+		#displayhelp()
+		print(colored('ERROR :','yellow','on_red')+colored(E,'yellow','on_red'))
+		print()
+	line = '-'*30
+	cprint(line,'green')
+	cprint("Thank you for using our script",'green')
+	cprint(line,'green')
+	print()
+	
 if __name__ == "__main__":
     main()
