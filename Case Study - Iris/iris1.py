e8df2f949235f2c594ef6c3e715e477b8a19bd38
Kaustubh Khairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2022-06-06 13:18:08 +0530

    Create iris1.py

---
+++ Case Study - Iris/iris1.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+print("Feature names of iris dataset")
+print(iris.feature_names)
+
+print("Target names of dataset")
+print(iris.target_names)
+
+print("First 10 elements od dataset")
+
+for i in range(10):
+    print("ID :%d , Label: %s , Feature : %s"%(i,iris.data[i],iris.target[i]))
