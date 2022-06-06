6cea401535f81ce63122c2499b68a290c00c3ae0
Kaustubh Khairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2022-06-06 13:38:54 +0530

    Update and rename Assignment1/Assignment1_9.py to Python Basics/Assignment9.py

--- Python Basics/Assignment9.py
+++ Python Basics/Assignment9.py
@@ -1,10 +1,9 @@
 # 9. Write a program which display first 10 even numbers on screen.
 
-def fun():
+def main():
     for n in range(1, 21):
         if n % 2 == 0:
             print(n)
 
-
 if __name__ == "__main__":
-    fun()
+    main()
