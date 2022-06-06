c29ecc55715c825228a4ffbcff2e7158dcd60284
Kaustubh Khairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2022-06-06 14:42:40 +0530

    Update and rename Assignment7/Assignment7_3.py to Object Orientation/Assignment6.py

--- Object Orientation/Assignment6.py
+++ Object Orientation/Assignment6.py
@@ -1,4 +1,4 @@
-#3. Write a program which contains one class named as Numbers.
+#6. Write a program which contains one class named as Numbers.
 #Arithmetic class contains one instance variables as Value.
 #Inside init method initialise that instance variables to the value which is accepted from user.
 #There are four instance methods inside class as ChkPrime(), ChkPerfect(), SumFactors(),
@@ -13,58 +13,58 @@
 class Numbers:
 
     def __init__(self,no):
-        self.value=no ;
+        self.value=no
 
     def ChkPrime(self):
-        flag = 0;
+        flag = 0
         for i in range(2, self.value):
             if (self.value % i == 0):
-                flag = 1;
+                flag = 1
                 break;
         if (flag == 1):
-            return False;
+            return False
         else:
-            return True;
+            return True
 
     def ChkPerfect(self):
-        res = Numbers.SumFactors(self);
+        res = Numbers.SumFactors(self)
         if(res== self.value):
-            return True;
+            return True
 
     def SumFactors(self):
-        sum=0;
-        fact = Numbers.Factors(self);
+        sum=0
+        fact = Numbers.Factors(self)
         for i in range(len(fact)):
-            sum = sum + fact[i];
+            sum = sum + fact[i]
         return sum;
 
     def Factors(self):
-        arr=list();
+        arr=list()
         for i in range(1,self.value):
             if(self.value%i==0):
-                arr.append(i);
-        return arr;
+                arr.append(i)
+        return arr
 
 def main():
-    no = input("Enter Number : ");
-    obj = Numbers(int(no));
-    ret1 = obj.ChkPrime();
+    no = input("Enter Number : ")
+    obj = Numbers(int(no))
+    ret1 = obj.ChkPrime()
     if(ret1==True):
         print (no,"is prime number.")
     else:
         print (no, "is not a prime number.")
 
-    ret2 = obj.ChkPerfect();
+    ret2 = obj.ChkPerfect()
     if (ret2 == True):
         print (no, "is perfect number.")
     else:
         print (no, "is not a perfect number.")
 
-    ret3 = obj.SumFactors();
-    print ("Sum of factors is ",ret3);
+    ret3 = obj.SumFactors()
+    print ("Sum of factors is ",ret3)
 
-    ret4 = obj.Factors();
-    print("Factors of ",no,"are",ret4);
+    ret4 = obj.Factors()
+    print("Factors of ",no,"are",ret4)
 
 if __name__  == "__main__":
-    main();
+    main()
