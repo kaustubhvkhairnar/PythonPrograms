7c6c925129c7da7ddedfe5fee050de7b953f047e
Kaustubh Khairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2022-06-06 14:31:23 +0530

    Update Assignment5.py

--- List/Assignment5.py
+++ List/Assignment5.py
@@ -1,9 +1,9 @@
 #5.Write a program which accept N numbers from user and store it into List. Return addition of all
 #prime numbers from that List. Main python file accepts N numbers from user and pass each
-#number to ChkPrime() function which is part of our user defined module named as
-#MarvellousNum. Name of the function from main python file should be ListPrime().
+#number to chkPrime() function which is part of our user defined module named as
+#Prime. Name of the function from main python file should be listPrime().
 
-from Num import *;
+from Prime import *;
 
 
 def main():
