c8922013ace4f0312239b09d186c593b4980ee5d
Kaustubh Khairnar <53002588+kaustubhvkhairnar@users.noreply.github.com>
2022-06-06 14:41:36 +0530

    Update and rename Assignment7/Assignment7_2.py to Object Orientation/Assignment5.py

--- Object Orientation/Assignment5.py
+++ Object Orientation/Assignment5.py
@@ -1,4 +1,4 @@
-#2. Write a program which contains one class named as BankAccount.
+#5. Write a program which contains one class named as BankAccount.
 #BankAccount class contains two instance variables as Name & Amount.
 #That class contains one class variable as ROI which is initialise to 10.5.
 #Inside init method initialise all name and amount variables by accepting the values from user.
@@ -18,33 +18,33 @@ class BankAccount:
     ROI = 10.5
 
     def __init__(self,value1,value2):
-        self.Name = value1;
-        self.Amount = value2;
+        self.Name = value1
+        self.Amount = value2
 
     def Display(self):
-        print("Name : ",self.Name);
-        print("Amount : ",self.Amount);
+        print("Name : ",self.Name)
+        print("Amount : ",self.Amount)
 
     def Deposit(self,value):
-        self.Amount = self.Amount+value;
+        self.Amount = self.Amount+value
 
     def Withdraw(self,value):
-        self.Amount = self.Amount - value;
+        self.Amount = self.Amount - value
 
     def CalculateInterest(self,value):
         Rate = value + BankAccount.ROI//100
-        print("Rate of interest for ",value,"is",Rate);
+        print("Rate of interest for ",value,"is",Rate)
 
 def main():
-    obj = BankAccount("Kaustubh",int(200000));
-    obj.Display();
-    obj.Deposit(int(200));
+    obj = BankAccount("Kaustubh",int(200000))
+    obj.Display()
+    obj.Deposit(int(200))
     print("Deposting")
-    obj.Display();
-    obj.Withdraw(int(300));
+    obj.Display()
+    obj.Withdraw(int(300))
     print("Withdrawing")
-    obj.Display();
-    obj.CalculateInterest(int(30000));
+    obj.Display()
+    obj.CalculateInterest(int(30000))
 
 if __name__=="__main__":
-    main();
+    main()
